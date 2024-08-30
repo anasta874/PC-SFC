39b2374f218ce859d0dd0cfcd6e580f8d306e0b6
Anasta Pp <anasta874@gmail.com>
2024-08-30 14:04:49 +0200

    update

--- XVG_MD_Trajectory_Visualization/file_paths.py
+++ XVG_MD_Trajectory_Visualization/file_paths.py
@@ -7,25 +7,26 @@ rmsd_files = {
     "P720Q": "/../../rmsd-P720Q.xvg",
     "S719del": "/../../rmsd-S719del.xvg",
     "PD720-721R": "/../../rmsd-PD720-721R.xvg",
-    "T739A": "/../../RMSD-all/rmsd-T739A.xvg"
+    "T739A": "/../../rmsd-T739A.xvg"
 }
 
 rmsf_files = {
-   "WT": "/../../rmsd-lf.xvg",
-    "S718C": "/../../rmsd-S718C.xvg",
-    "P720R": "/../../rmsd-P720R.xvg",
-    "P720Q": "/../../rmsd-P720Q.xvg",
-    "S719del": "/../../rmsd-S719del.xvg",
-    "PD720-721R": "/../../rmsd-PD720-721R.xvg",
-    "T739A": "/../../RMSD-all/rmsd-T739A.xvg"
+   "WT": "/../../rmsf-lf.xvg",
+    "S718C": "/../../rmsf-S718C.xvg",
+    "P720R": "/../../rmsf-P720R.xvg",
+    "P720Q": "/../../rmsf-P720Q.xvg",
+    "S719del": "/../../rmsf-S719del.xvg",
+    "PD720-721R": "/../../rmsf-PD720-721R.xvg",
+    "T739A": "/../../rmsf-T739A.xvg"
 }
 
 rg_files = {
-    "WT": "/../../rmsd-lf.xvg",
-    "S718C": "/../../rmsd-S718C.xvg",
-    "P720R": "/../../rmsd-P720R.xvg",
-    "P720Q": "/../../rmsd-P720Q.xvg",
-    "S719del": "/../../rmsd-S719del.xvg",
-    "PD720-721R": "/../../rmsd-PD720-721R.xvg",
-    "T739A": "/../../RMSD-all/rmsd-T739A.xvg"
+    "WT": "/../../rg-lf.xvg",
+    "S718C": "/../../rg-S718C.xvg",
+    "P720R": "/../../rg-P720R.xvg",
+    "P720Q": "/../../rg-P720Q.xvg",
+    "S719del": "/../../rg-S719del.xvg",
+    "PD720-721R": "/../../rg-PD720-721R.xvg",
+    "T739A": "/../../rg-T739A.xvg"
 }
+
